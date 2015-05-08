736f2f9193b1ce1a727c6b3c27b380fe0864609d
Marek Ladicky <marek.ladicky@gmail.com>
2015-05-08 02:36:16 +0200

    Update

--- www/js/index.js
+++ www/js/index.js
@@ -31,3 +31,35 @@ var app = {
 };
 
 app.initialize();
+
+var watchID = null;
+
+function startWatch(p) {
+	var options = { frequency: 2000 };  // Update every 2 seconds
+	watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+	var watchingElement = p.parentElement.querySelector('.watching');
+	p.setAttribute('style', 'display:none;');
+	watchingElement.setAttribute('style', 'display:block;');
+};
+
+function stopWatch(p) {
+    if (watchID) {
+        navigator.accelerometer.clearWatch(watchID);
+        watchID = null;
+        var receivedElement = p.parentElement.querySelector('.received');
+        p.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+    }
+};
+
+function onSuccess(acceleration) {
+    var resultPane = document.getElementById('resultPane');
+    resultPane.innerHTML = 'Acceleration X: ' + acceleration.x + "&lt;br />";
+    resultPane.innerHTML += 'Acceleration Y: ' + acceleration.y + "&lt;br />";
+    resultPane.innerHTML += 'Acceleration Z: ' + acceleration.z + "&lt;br />";
+    resultPane.innerHTML += 'Timestamp: ' + acceleration.timestamp + "&lt;br />";
+};
+
+function onError() {
+	alert('onError!');
+};
